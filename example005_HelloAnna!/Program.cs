b6164d645eabfe5822a961a75486e8b4559d9d8b
Anna <anek_ogonek@mail.ru>
2022-11-25 22:54:27 +0300

    added project example007_Cycle

--- example005_HelloAnna!/Program.cs
+++ example005_HelloAnna!/Program.cs
@@ -1,6 +1,6 @@
 ﻿Console.Write("Введите Ваше имя");
 string username = Console.ReadLine();
-if(username.ToLower() == "анна")
+if(username == "Анна")
 {
 Console.WriteLine("Ура!Это же Анна!");    
 }
